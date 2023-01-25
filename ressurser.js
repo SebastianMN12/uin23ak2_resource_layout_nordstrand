da5eb9c83b838a56169c39e940f683802202903f
SebastianMN12 <sebastian.m.nordstrand@hotmail.no>
2023-01-25 17:58:20 +0100

    3

--- ressurser.js
+++ ressurser.js
@@ -16,6 +16,7 @@ const resources = [
                 url: "https://html.com/"
             },
         ]
+        
     },
     {
         category: "CSS",
@@ -94,3 +95,25 @@ const resources = [
         ]
     },
 ]
+
+function openres(evt, resName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(resName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
